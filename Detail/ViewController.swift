76b365fab33f34883cb76496921b541d5d7c6510
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-12-14 13:36:52 +0100

    Adds a title label and a price label

--- Detail/ViewController.swift
+++ Detail/ViewController.swift
@@ -2,12 +2,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let margin: CGFloat = 16
+    let fontSize: CGFloat = 26
+
     lazy var galleryImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         imageView.image = #imageLiteral(resourceName: "sofa2")
         return imageView
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isAccessibilityElement = true
+        label.font = UIFont.systemFont(ofSize: fontSize)
+        label.numberOfLines = 0
+        label.text = "Ny lekker sofa fra Ygg og Lybg selges til halv pris!!!"
+        label.accessibilityLabel = label.text
+        return label
+    }()
+
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isAccessibilityElement = true
+        label.font = UIFont.boldSystemFont(ofSize: fontSize)
+        label.text = "500,-"
+        label.accessibilityLabel = "Pris: 500kroner"
+        return label
     }()
 
     override func viewDidLoad() {
@@ -20,12 +44,22 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(galleryImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(priceLabel)
 
         NSLayoutConstraint.activate([
             galleryImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
             galleryImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
             galleryImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
             galleryImageView.heightAnchor.constraint(equalToConstant: 200),
+
+            titleLabel.topAnchor.constraint(equalTo: galleryImageView.bottomAnchor, constant: margin),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
+
+            priceLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            priceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            priceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
         ])
     }
     }
