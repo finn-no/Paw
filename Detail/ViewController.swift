9b4991fa20372169b7a932067eb75898f5ec310e
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-12-14 13:35:38 +0100

    Adds a gallery image view

--- Detail/ViewController.swift
+++ Detail/ViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    lazy var galleryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = #imageLiteral(resourceName: "sofa2")
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -8,6 +17,17 @@ class ViewController: UIViewController {
     }
 
     func setup() {
+        view.backgroundColor = .white
+        
+        view.addSubview(galleryImageView)
+
+        NSLayoutConstraint.activate([
+            galleryImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
+            galleryImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            galleryImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
+            galleryImageView.heightAnchor.constraint(equalToConstant: 200),
+        ])
+    }
     }
 }
 
