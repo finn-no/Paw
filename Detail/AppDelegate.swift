386917bdf73a00fd9538e6a6fc52d850401206b3
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-12-14 12:50:54 +0100

    Uses the optional window and sets initial view controller

--- Detail/AppDelegate.swift
+++ Detail/AppDelegate.swift
@@ -6,10 +6,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController()
+        let viewController = ViewController()
 
-        window.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
 
         return true
     }
