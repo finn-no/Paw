77aca86a294fc7820f9fa12e7001dcc6ffb55aaa
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2017-12-22 13:49:00 +0100

    Lays out subviews in content view and populates the link components

--- Detail/ObjectView.swift
+++ Detail/ObjectView.swift
@@ -53,16 +53,44 @@ class ObjectView: UIView {
     }
 
     func reloadData() {
-        for subview in subviews {
+        for subview in contentView.subviews {
             subview.removeFromSuperview()
         }
 
         let components = dataSource?.componentsInObjectView(self) ?? [Component]()
-        for component in components {
+        var previousLinkComponent = LinkComponentView()
+        for (index, component) in components.enumerated() {
             if component.type == "link" {
                 // add each as subview and lay out
                 let linkComponent = LinkComponentView()
                 linkComponent.delegate = self
+                linkComponent.component = component
+
+                contentView.addSubview(linkComponent)
+
+                linkComponent.setupLayout()
+
+                NSLayoutConstraint.activate([
+                    linkComponent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: .mediumLargeSpacing),
+                    linkComponent.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -.mediumLargeSpacing),
+                ])
+
+                switch index {
+                case 0:
+                    NSLayoutConstraint.activate([
+                        linkComponent.topAnchor.constraint(equalTo: contentView.topAnchor, constant: .mediumLargeSpacing),
+                    ])
+                case components.count-1:
+                    NSLayoutConstraint.activate([
+                        linkComponent.topAnchor.constraint(equalTo: previousLinkComponent.bottomAnchor, constant: .mediumLargeSpacing),
+                        linkComponent.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -.mediumLargeSpacing),
+                    ])
+                default:
+                    NSLayoutConstraint.activate([
+                        linkComponent.topAnchor.constraint(equalTo: previousLinkComponent.bottomAnchor, constant: .mediumLargeSpacing),
+                    ])
+                }
+                previousLinkComponent = linkComponent
             }
         }
     }
