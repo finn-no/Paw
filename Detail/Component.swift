bc4be47a425a7c5cc59949ecfe5f810cc2683442
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-11 13:34:42 +0100

    Simplifies the component protocol

--- Detail/Component.swift
+++ Detail/Component.swift
@@ -1,33 +1,3 @@
-import Foundation
-
-enum ComponentType {
-    case link
-    case title
-    case custom
-    case gallery
-    case profile
-    case price
-    case messageButton
-    case phoneNumber
-    case adress
-    case description
-    case category
-    case banner
-    case safePay
-    case loanPrice
-    case deliveryHelp
-    case adReporter
-    case adInfo
-    case relevantAds
-}
-
 protocol Component {
     var id: String { get }
-    var type: ComponentType { get }
-    var actions: [ComponentAction] { get }
-}
-
-enum ComponentAction {
-    case show
-    case call
 }
