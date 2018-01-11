e30663882c9940cc26bbf53228595140b24eb3fd
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-11 15:54:28 +0100

    Removes entry for CollapsableDescriptionComponent

--- Components/ViewComponents/CollapsableDescription/CollapsableDescriptionComponent.swift
+++ Components/ViewComponents/CollapsableDescription/CollapsableDescriptionComponent.swift
@@ -7,13 +7,11 @@ struct CollapsableDescriptionComponent: Component {
     let text: String
     let titleShow: String
     let titleHide: String
-    let accessibilityLabelPrefix: String
 
-    init(id: String = UUID().uuidString, text: String, titleShow: String, titleHide: String, accessibilityLabelPrefix: String) {
+    init(id: String = UUID().uuidString, text: String, titleShow: String, titleHide: String) {
         self.id = id
         self.text = text
         self.titleShow = titleShow
         self.titleHide = titleHide
-        self.accessibilityLabelPrefix = accessibilityLabelPrefix
     }
 }
