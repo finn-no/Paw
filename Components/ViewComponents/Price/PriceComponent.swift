5ab1a7b3debe8d0c364e4bcdda48b382ef9f4b7e
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-22 13:16:47 +0100

    Sets up the component propoerties

--- Components/ViewComponents/Price/PriceComponent.swift
+++ Components/ViewComponents/Price/PriceComponent.swift
@@ -3,10 +3,14 @@ import Foundation
 struct PriceComponent: Component {
 
     let id: String
-    let price: Double
+    let price: Int
+    let currency: String
+    let accessibilityPrefix: String
 
-    init(id: String = UUID().uuidString, price: Double) {
+    init(id: String = UUID().uuidString, price: Int, currency: String, accessibilityPrefix: String) {
         self.id = id
         self.price = price
+        self.currency = currency
+        self.accessibilityPrefix = accessibilityPrefix
     }
 }
