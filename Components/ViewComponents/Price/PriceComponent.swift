c1599da81547e4f7de076321428cf207e9f2e258
Elvis Nuñez <elvisnunez@me.com>
2018-01-26 09:25:00 +0100

    Make price a Double

--- Components/ViewComponents/Price/PriceComponent.swift
+++ Components/ViewComponents/Price/PriceComponent.swift
@@ -3,12 +3,12 @@ import Foundation
 struct PriceComponent: Component {
 
     let id: String
-    let price: Int
+    let price: Double
     let locale: Locale
     let accessibilityPrefix: String
     let status: String?
 
-    init(id: String = UUID().uuidString, price: Int, locale: Locale, accessibilityPrefix: String, status: String? = nil) {
+    init(id: String = UUID().uuidString, price: Double, locale: Locale, accessibilityPrefix: String, status: String? = nil) {
         self.id = id
         self.price = price
         self.locale = locale
