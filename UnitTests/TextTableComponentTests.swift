5ff4b7d97e97159105ec2139cfaac4f4c6c4ce9e
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-29 08:41:27 +0100

    Adds unit testing for text table component

---
+++ UnitTests/TextTableComponentTests.swift
@@ -0,0 +1,26 @@
+import Foundation
+import XCTest
+@testable import Components
+
+class TextTableComponentTests: XCTestCase {
+    let titleString = "FINN-kode"
+    let detailString = "123456789"
+
+    func testPriceLabel() {
+        let textTableComponent = TextTableComponent(title: titleString, detail: detailString)
+
+        XCTAssertEqual(textTableComponent.title, titleString)
+    }
+
+    func testDescriptionLabel() {
+        let textTableComponent = TextTableComponent(title: titleString, detail: detailString)
+
+        XCTAssertEqual(textTableComponent.detail, detailString)
+    }
+
+    func testAccessibilityLabel() {
+        let textTableComponent = TextTableComponent(title: titleString, detail: detailString)
+
+        XCTAssertEqual(textTableComponent.accessibilityLabel, titleString + ": " + detailString)
+    }
+}
