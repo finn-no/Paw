3ba9f167c4eee182fa1abdf95adbeb6b64438aae
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-30 15:00:43 +0100

    Update the expected string in unit testing of american price

--- UnitTests/PriceComponentTests.swift
+++ UnitTests/PriceComponentTests.swift
@@ -20,9 +20,9 @@ class PriceComponentTests: XCTestCase {
         let smallAmericanComponent = PriceComponent(price: smallPrice, locale: americanLocale, accessibilityPrefix: americanPriceString)
 
         XCTAssertEqual(largeNorwegianComponent.priceLabel, "kr 1 500 000,-")
-        XCTAssertEqual(largeAmericanComponent.priceLabel, "$ 1500000,-")
+        XCTAssertEqual(largeAmericanComponent.priceLabel, "$1,500,000,-")
         XCTAssertEqual(smallNorwegianComponent.priceLabel, "kr 0,-")
-        XCTAssertEqual(smallAmericanComponent.priceLabel, "$ 0,-")
+        XCTAssertEqual(smallAmericanComponent.priceLabel, "$0,-")
     }
 
     func testAccessibilityLabel() {
