21fc1658a505368d48e9e10fb12506be0839b358
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-01-30 12:18:39 +0100

    Adds unit tests for date label and accessibility label

--- UnitTests/DateTableComponentTests.swift
+++ UnitTests/DateTableComponentTests.swift
@@ -6,16 +6,10 @@ class DateTableComponentTests: XCTestCase {
     let titleString = "Sist endret"
     let locale = Locale(identifier: "no_NO")
     let dateAndTimeString = "6. apr. 2015, 19:16"
-    let dateString = "6. april 2015"
-    let timeString = "19:16"
+    let dateString = "mandag 6. april 2015"
+    let timeString = "19:16:40 CEST"
     let yearString = "2015"
     let timeInterval = TimeInterval(exactly: 450033400)!
-
-    lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = locale
-        return formatter
-    }()
 
     func testTitleLabel() {
         let date = Date(timeIntervalSinceReferenceDate: timeInterval)
@@ -26,21 +20,28 @@ class DateTableComponentTests: XCTestCase {
     }
 
     func testDateLabel() {
-//        let date = Date(timeIntervalSinceReferenceDate: timeInterval)
-//        let dateAndTimeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .dateAndTime)
-//        let dateTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .date)
-//        let timeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .time)
-//        let yearTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .year)
-//
-//        XCTAssertEqual(dateAndTimeTableComponent.date, dateFormatter.date(from: dateAndTimeString))
-//        XCTAssertEqual(dateTableComponent.date, dateFormatter.date(from: dateString))
-//        XCTAssertEqual(timeTableComponent.date, dateFormatter.date(from: timeString))
-//        XCTAssertEqual(yearTableComponent.date, dateFormatter.date(from: yearString))
+        let date = Date(timeIntervalSinceReferenceDate: timeInterval)
+        let dateAndTimeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .dateAndTime)
+        let dateTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .date)
+        let timeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .time)
+        let yearTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .year)
+
+        XCTAssertEqual(dateAndTimeTableComponent.dateLabel, dateAndTimeString)
+        XCTAssertEqual(dateTableComponent.dateLabel, dateString)
+        XCTAssertEqual(timeTableComponent.dateLabel, timeString)
+        XCTAssertEqual(yearTableComponent.dateLabel, yearString)
     }
 
     func testAccessibilityLabel() {
-//        let textTableComponent = TextTableComponent(title: titleString, detail: detailString)
-//
-//        XCTAssertEqual(textTableComponent.accessibilityLabel, titleString + ": " + detailString)
+        let date = Date(timeIntervalSinceReferenceDate: timeInterval)
+        let dateAndTimeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .dateAndTime)
+        let dateTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .date)
+        let timeTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .time)
+        let yearTableComponent = DateTableComponent(title: titleString, date: date, locale: locale, dateFormat: .year)
+
+        XCTAssertEqual(dateAndTimeTableComponent.accessibilityLabel, titleString + ": " + dateAndTimeString)
+        XCTAssertEqual(dateTableComponent.accessibilityLabel, titleString + ": " + dateString)
+        XCTAssertEqual(timeTableComponent.accessibilityLabel, titleString + ": " + timeString)
+        XCTAssertEqual(yearTableComponent.accessibilityLabel, titleString + ": " + yearString)
     }
 }
