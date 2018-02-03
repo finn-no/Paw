8d62b8c195aa1e36b4b6923dd956287f1ae873a0
Elvis Nuñez <elvisnunez@me.com>
2018-02-03 21:02:06 +0100

    Add gallery component

---
+++ Sources/Components/Gallery/GalleryComponent.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct GalleryComponent: Component {
+    public let id: String
+
+    public init(id: String = UUID().uuidString) {
+        self.id = id
+    }
+GalleryComponent}
+
---
+++ Sources/Components/Gallery/GalleryComponentView.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+public class GalleryComponentView: UIView {
+    var component: GalleryComponent? {
+        didSet {
+            guard let component = component else {
+                return
+            }
+        }
+    }
+
+    // MARK: - Setup
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        addSubview(priceLabel)
+
+        isAccessibilityElement = true
+
+        NSLayoutConstraint.activate([
+            priceLabel.topAnchor.constraint(equalTo: topAnchor),
+            priceLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            priceLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            ])
+    }
+}
