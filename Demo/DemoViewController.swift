1c3a7a8fa41a5e848793ffe58e478514fc8155f8
Elvis Nuñez <elvisnunez@me.com>
2018-02-04 21:49:20 +0100

    Rename IconButtonComponent into LinkComponent

--- Demo/DemoViewController.swift
+++ Demo/DemoViewController.swift
@@ -33,8 +33,8 @@ class DemoViewController: UIViewController {
         return [
             [CallToActionButtonComponent(title: "Send melding", subtitle: "Svarer vanligvis innen 4 timer")],
             [PhoneNumberComponent(phoneNumber: "12345678", descriptionText: "Mobil", showNumberText: "Vis telefonnummer", accessibilityLabelPrefix: "Telefonnummer: ")],
-            [IconButtonComponent(buttonTitle: "Hans Nordahls gate 64, 0841 Oslo", iconImage: pinImage!)],
-            [IconButtonComponent(buttonTitle: "Få hjelp til frakt", iconImage: vanImage!)],
+            [LinkComponent(title: "Hans Nordahls gate 64, 0841 Oslo", iconImage: pinImage!)],
+            [LinkComponent(title: "Få hjelp til frakt")],
             [CollapsableDescriptionComponent(text: attributedDescriptionText, titleShow: "+ Vis hele beskrivelsen", titleHide: "- Vis mindre")],
             [CallToActionButtonComponent(title: "Send melding", subtitle: "Svarer vanligvis innen 4 timer"), CallToActionButtonComponent(title: "Ring")],
             [PriceComponent(price: 1_500_000, locale: locale, accessibilityPrefix: "Pris: ")],
@@ -85,7 +85,7 @@ class DemoViewController: UIViewController {
 
         smashView.phoneNumberDelegate = self
         smashView.callToActionButtonDelegate = self
-        smashView.iconButtonDelegate = self
+        smashView.linkDelegate = self
 
         smashView.reloadData()
     }
@@ -132,8 +132,8 @@ extension DemoViewController: CallToActionButtonSmashViewDelegate {
     }
 }
 
-extension DemoViewController: IconButtonSmashViewDelegate {
-    func smashView(_ smashView: SmashView, didTapButtonFor component: IconButtonComponent) {
+extension DemoViewController: LinkSmashViewDelegate {
+    func smashView(_ smashView: SmashView, didTapButtonFor component: LinkComponent) {
         let alert = UIAlertController.dismissableAlert(title: "Button with id: \(component.id)")
         present(alert, animated: true, completion: nil)
     }
